097b865014a1efeee807a6722ce106a8d88998d1
ankur-tutlani <89307618+ankur-tutlani@users.noreply.github.com>
2024-04-14 09:32:14 +0530

    Add files via upload

--- multi_agent_decision/simulation.py
+++ multi_agent_decision/simulation.py
@@ -1,3 +1,4 @@
+
 import numpy as np   
 import pandas as pd
 from collections import Counter
@@ -11,9 +12,8 @@ import itertools
 
 
 
+
 dotprod = lambda K, L: reduce(lambda z1, z2: z1+z2, map(lambda x: reduce(lambda x1, x2: x1*x2, x), zip(K, L))) if len(K)==len(L) else 0
-
-
 
 # 1.random_seed : random seed to reproduce results. Integer
 # 2.iteration_name: give any name for this iterations. String
@@ -68,7 +68,6 @@ dotprod = lambda K, L: reduce(lambda z1, z2: z1+z2, map(lambda x: reduce(lambda 
 
 
 
-
 def perturbed_response1(num_agents,agent_and_strategy_pd2,potential_edges,perturb_ratio,unique_strategies,fixed_agents,fixed_strategy_to_use):
     
     agent_strategy_to_choose=[]
@@ -110,28 +109,34 @@ def perturbed_response1(num_agents,agent_and_strategy_pd2,potential_edges,pertur
 
     else:
         for j in range(num_agents):
-            check1 = [i for i in potential_edges if i[0] == j]
-            check2 = [i for i in potential_edges if i[1] == j]
-            check1.extend(check2)
-            flat_list1 = list(set([item for sublist in check1 for item in sublist]))
-            data_to_check = agent_and_strategy_pd2.loc[agent_and_strategy_pd2['agent_no'].isin(flat_list1)].reset_index(drop=True)
+            try:
+                check1 = [i for i in potential_edges if i[0] == j]
+                check2 = [i for i in potential_edges if i[1] == j]
+                check1.extend(check2)
+                flat_list1 = list(set([item for sublist in check1 for item in sublist]))
+                data_to_check = agent_and_strategy_pd2.loc[agent_and_strategy_pd2['agent_no'].isin(flat_list1)].reset_index(drop=True)
 
-            if data_to_check.loc[data_to_check['agent_no']==j]['payoff'].values[0] == max(data_to_check['payoff']):
-                xx = data_to_check.loc[data_to_check['agent_no']==j]['strategy'].values[0]
-            else:
-                xx = list(set(data_to_check.loc[(data_to_check['agent_no']!=j) & (data_to_check['payoff']==max(data_to_check['payoff']))]['strategy'].tolist()))
-                xx = random.choices(population=xx,k=1)[0]
+                if data_to_check.loc[data_to_check['agent_no']==j]['payoff'].values[0] == max(data_to_check['payoff']):
+                    xx = data_to_check.loc[data_to_check['agent_no']==j]['strategy'].values[0]
+                else:
+                    xx = list(set(data_to_check.loc[(data_to_check['agent_no']!=j) & (data_to_check['payoff']==max(data_to_check['payoff']))]['strategy'].tolist()))
+                    xx = random.choices(population=xx,k=1)[0]
 
-            non_recommended = [k for k in unique_strategies if k != xx]
+                non_recommended = [k for k in unique_strategies if k != xx]
 
-            if len(non_recommended) > 0:
-                draw2= random.choices(population=non_recommended,k=1)
-                best_response = random.choices(population=[xx,draw2[0]],weights=[1-perturb_ratio,perturb_ratio],k=1)
-                best_response = best_response[0]
-            else:
-                best_response = xx
+                if len(non_recommended) > 0:
+                    draw2= random.choices(population=non_recommended,k=1)
+                    best_response = random.choices(population=[xx,draw2[0]],weights=[1-perturb_ratio,perturb_ratio],k=1)
+                    best_response = best_response[0]
+                else:
+                    best_response = xx
 
-            agent_strategy_to_choose.append(best_response)
+                agent_strategy_to_choose.append(best_response)
+            
+            except:
+                
+                agent_strategy_to_choose.append(agent_and_strategy_pd2.loc[agent_and_strategy_pd2['agent_no']==j]['strategy'].values[0])
+
 
     return agent_strategy_to_choose
 
@@ -179,32 +184,38 @@ def perturbed_response2(num_agents,agent_and_strategy_pd2,potential_edges,pertur
 
     else:
         for j in range(num_agents):
-            check1 = [i for i in potential_edges if i[0] == j]
-            check2 = [i for i in potential_edges if i[1] == j]
-            check1.extend(check2)
-            flat_list1 = list(set([item for sublist in check1 for item in sublist]))
-            flat_list1.remove(j) ## to get opponent's agents 
+            try:
+                
+                check1 = [i for i in potential_edges if i[0] == j]
+                check2 = [i for i in potential_edges if i[1] == j]
+                check1.extend(check2)
+                flat_list1 = list(set([item for sublist in check1 for item in sublist]))
+                flat_list1.remove(j) ## to get opponent's agents 
 
-            ### this is aggregated data..
-            percent_share_data =agent_and_strategy_pd2.loc[agent_and_strategy_pd2['agent_no'].isin(flat_list1)].reset_index(drop=True)['strategy'].value_counts(normalize=True).to_frame()
-            percent_share_data.columns = ['prob']
-            percent_share_data['strategy'] = percent_share_data.index.values
-            percent_share_data['prob'] = np.round(percent_share_data['prob'],2)
-            prob_opponent_strategy = []
-            for i in unique_strategies: ### should be in 0,1,2,,,, order 
-                try:
-                    xx = percent_share_data.loc[percent_share_data['strategy']==i]['prob'].values[0]
-                except:
-                    xx = 0
-                prob_opponent_strategy.append(xx)
+                ### this is aggregated data..
+                percent_share_data =agent_and_strategy_pd2.loc[agent_and_strategy_pd2['agent_no'].isin(flat_list1)].reset_index(drop=True)['strategy'].value_counts(normalize=True).to_frame()
+                percent_share_data.columns = ['prob']
+                percent_share_data['strategy'] = percent_share_data.index.values
+                percent_share_data['prob'] = np.round(percent_share_data['prob'],2)
+                prob_opponent_strategy = []
+                for i in unique_strategies: ### should be in 0,1,2,,,, order 
+                    try:
+                        xx = percent_share_data.loc[percent_share_data['strategy']==i]['prob'].values[0]
+                    except:
+                        xx = 0
+                    prob_opponent_strategy.append(xx)
 
-            agent_payoffs=[]
-            for i in unique_strategies:
-                agent_payoffs.append(dotprod(payoff_matrix[i,].tolist(),prob_opponent_strategy))
+                agent_payoffs=[]
+                for i in unique_strategies:
+                    agent_payoffs.append(dotprod(payoff_matrix[i,].tolist(),prob_opponent_strategy))
 
-            index_strategy = [index for index,item in enumerate(agent_payoffs) if item == max(agent_payoffs)]
-            agent_choice = random.choices(index_strategy,k=1)[0]
-            agent_strategy_to_choose.append(agent_choice)
+                index_strategy = [index for index,item in enumerate(agent_payoffs) if item == max(agent_payoffs)]
+                agent_choice = random.choices(index_strategy,k=1)[0]
+                agent_strategy_to_choose.append(agent_choice)
+            
+            except:
+                
+                agent_strategy_to_choose.append(agent_and_strategy_pd2.loc[agent_and_strategy_pd2['agent_no']==j]['strategy'].values[0])
 
     return agent_strategy_to_choose
 
@@ -245,24 +256,30 @@ def perturbed_response3(num_agents,agent_and_strategy_pd2,potential_edges,pertur
 
     else:
         for j in range(num_agents):
-            check1 = [i for i in potential_edges if i[0] == j]
-            check2 = [i for i in potential_edges if i[1] == j]
-            check1.extend(check2)
-            flat_list1 = list(set([item for sublist in check1 for item in sublist]))
-            data_to_check = agent_and_strategy_pd2.loc[agent_and_strategy_pd2['agent_no'].isin(flat_list1)].reset_index(drop=True)
+            try:
+                
+                check1 = [i for i in potential_edges if i[0] == j]
+                check2 = [i for i in potential_edges if i[1] == j]
+                check1.extend(check2)
+                flat_list1 = list(set([item for sublist in check1 for item in sublist]))
+                data_to_check = agent_and_strategy_pd2.loc[agent_and_strategy_pd2['agent_no'].isin(flat_list1)].reset_index(drop=True)
 
-            if data_to_check.loc[data_to_check['agent_no']==j]['payoff'].values[0] == max(data_to_check['payoff']):
-                xx = data_to_check.loc[data_to_check['agent_no']==j]['strategy'].values[0]
-            else:
-                xx = list(set(data_to_check.loc[(data_to_check['agent_no']!=j) & (data_to_check['payoff']==max(data_to_check['payoff']))]['strategy'].tolist()))
-                xx = random.choices(population=xx,k=1)[0]
+                if data_to_check.loc[data_to_check['agent_no']==j]['payoff'].values[0] == max(data_to_check['payoff']):
+                    xx = data_to_check.loc[data_to_check['agent_no']==j]['strategy'].values[0]
+                else:
+                    xx = list(set(data_to_check.loc[(data_to_check['agent_no']!=j) & (data_to_check['payoff']==max(data_to_check['payoff']))]['strategy'].tolist()))
+                    xx = random.choices(population=xx,k=1)[0]
 
 
-            draw2= random.choices(population=unique_strategies,k=1)
-            best_response = random.choices(population=[xx,draw2[0]],weights=[1-perturb_ratio,perturb_ratio],k=1)
-            best_response = best_response[0]
+                draw2= random.choices(population=unique_strategies,k=1)
+                best_response = random.choices(population=[xx,draw2[0]],weights=[1-perturb_ratio,perturb_ratio],k=1)
+                best_response = best_response[0]
 
-            agent_strategy_to_choose.append(best_response)
+                agent_strategy_to_choose.append(best_response)
+            
+            except:
+                agent_strategy_to_choose.append(agent_and_strategy_pd2.loc[agent_and_strategy_pd2['agent_no']==j]['strategy'].values[0])
+
 
     return agent_strategy_to_choose
 
@@ -293,21 +310,23 @@ def perturbed_response4(num_agents,agent_and_strategy_pd2,potential_edges,fixed_
                     agent_strategy_to_choose.append(random.choices(population=xx,k=1)[0])
 
     else:
-
         for j in range(num_agents):
+            try:
+                check1 = [i for i in potential_edges if i[0] == j]
+                check2 = [i for i in potential_edges if i[1] == j]
+                check1.extend(check2)
+                flat_list1 = list(set([item for sublist in check1 for item in sublist]))
+                data_to_check = agent_and_strategy_pd2.loc[agent_and_strategy_pd2['agent_no'].isin(flat_list1)].reset_index(drop=True)
 
-            check1 = [i for i in potential_edges if i[0] == j]
-            check2 = [i for i in potential_edges if i[1] == j]
-            check1.extend(check2)
-            flat_list1 = list(set([item for sublist in check1 for item in sublist]))
-            data_to_check = agent_and_strategy_pd2.loc[agent_and_strategy_pd2['agent_no'].isin(flat_list1)].reset_index(drop=True)
-
-            if data_to_check.loc[data_to_check['agent_no']==j]['payoff'].values[0] == max(data_to_check['payoff']):
-                agent_strategy_to_choose.append(data_to_check.loc[data_to_check['agent_no']==j]['strategy'].values[0])
-            else:
-                xx = list(set(data_to_check.loc[(data_to_check['agent_no']!=j) & (data_to_check['payoff']==max(data_to_check['payoff']))]['strategy'].tolist()))
-                agent_strategy_to_choose.append(random.choices(population=xx,k=1)[0])
-
+                if data_to_check.loc[data_to_check['agent_no']==j]['payoff'].values[0] == max(data_to_check['payoff']):
+                    agent_strategy_to_choose.append(data_to_check.loc[data_to_check['agent_no']==j]['strategy'].values[0])
+                else:
+                    xx = list(set(data_to_check.loc[(data_to_check['agent_no']!=j) & (data_to_check['payoff']==max(data_to_check['payoff']))]['strategy'].tolist()))
+                    agent_strategy_to_choose.append(random.choices(population=xx,k=1)[0])
+                    
+            except:
+                agent_strategy_to_choose.append(agent_and_strategy_pd2.loc[agent_and_strategy_pd2['agent_no']==j]['strategy'].values[0])
+                
     return agent_strategy_to_choose
 
 
@@ -380,17 +399,17 @@ def simulation_function_neighbors(random_seed,
     flat_list = [item for sublist in list_to_fill for item in sublist]
     if len(flat_list) == num_agents:
         pass
-    #         print("ok")
+    
     elif len(flat_list) < num_agents:
         delta = num_agents - len(flat_list)
         for kk in range(delta):
             flat_list.append(flat_list[-1])
-    #         print("last elements added")
+   
     else:
         delta = len(flat_list) - num_agents
         for kk in range(delta):
             flat_list.pop()
-    #         print("last elements deleted")
+    
 
     if len(agent_initial_state) > 0:
         samplelist1 = agent_initial_state
@@ -422,6 +441,10 @@ def simulation_function_neighbors(random_seed,
     
     potential_edges = list(G.edges)
     
+    agents_in_edges = list(set([i[0] for i in potential_edges]+[i[1] for i in potential_edges]))
+    agents_in_network = list(range(num_agents))
+    delta_agents_hardcoded = [i for i in agents_in_network if i not in agents_in_edges]
+    
     agents_list = list(range(num_agents))
     agent_and_strategy_pd = pd.DataFrame({'agent_no':agents_list,'strategy':samplelist1})
     intial_states_pd = pd.DataFrame([{'initial_state':str([thisdict[k] for k in samplelist1])}],columns=['initial_state'])
@@ -436,7 +459,7 @@ def simulation_function_neighbors(random_seed,
     unique_strategies = list(range(num_strategies))
     
     
-    ### initial state graph
+    
     names_to_check=unique_strategies
     get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF),range(n)))
     list_of_colors = get_colors(len(names_to_check))
@@ -461,17 +484,20 @@ def simulation_function_neighbors(random_seed,
 
         agent_payoffs_to_store = []  
         for j in range(num_agents):
-            check1 = [i for i in potential_edges if i[0] == j]
-            check2 = [i for i in potential_edges if i[1] == j]
-            check1.extend(check2)
-            flat_list1 = list(set([item for sublist in check1 for item in sublist]))
-            flat_list1.remove(j)
-            row_strategy = agent_and_strategy_pd.loc[agent_and_strategy_pd['agent_no'] == j]['strategy'].values[0]
-            data_to_check = agent_and_strategy_pd.loc[agent_and_strategy_pd['agent_no'].isin(flat_list1)].reset_index(drop=True)
-            agent_payoff = 0
-            for i in range(len(data_to_check)):
-                agent_payoff += payoff_matrix[row_strategy,data_to_check.iloc[i]['strategy']] 
-            agent_payoffs_to_store.append(agent_payoff)
+            try:
+                check1 = [i for i in potential_edges if i[0] == j]
+                check2 = [i for i in potential_edges if i[1] == j]
+                check1.extend(check2)
+                flat_list1 = list(set([item for sublist in check1 for item in sublist]))
+                flat_list1.remove(j)
+                row_strategy = agent_and_strategy_pd.loc[agent_and_strategy_pd['agent_no'] == j]['strategy'].values[0]
+                data_to_check = agent_and_strategy_pd.loc[agent_and_strategy_pd['agent_no'].isin(flat_list1)].reset_index(drop=True)
+                agent_payoff = 0
+                for i in range(len(data_to_check)):
+                    agent_payoff += payoff_matrix[row_strategy,data_to_check.iloc[i]['strategy']] 
+                agent_payoffs_to_store.append(agent_payoff)
+            except:
+                agent_payoffs_to_store.append(None)
 
 
         agent_and_strategy_payoff = pd.DataFrame({'agent_no':agents_list,'payoff':agent_payoffs_to_store})
@@ -495,9 +521,9 @@ def simulation_function_neighbors(random_seed,
         data1_to_store = data1_to_store.replace({"strategy":thisdict})
         data1_to_store["timeperiod_number"] = timeperiod
         data1_to_store["starting_position"] = str([thisdict[k] for k in samplelist1])
-        trend_db_to_store = trend_db_to_store.append(data1_to_store,ignore_index=True)
+        trend_db_to_store = pd.concat([trend_db_to_store,data1_to_store],ignore_index=True)
 
-        #### added ###
+        
         if enable_delta_payoff == 'Yes' and timeperiod > min_time_period and max(trend_db_to_store['percent_count']) >= norms_agents_frequency:
             norms_to_store = []
             percent_time_frequency = []
@@ -535,11 +561,11 @@ def simulation_function_neighbors(random_seed,
     payoff_matrices_pd.columns = ['payoff_matrix']
     payoff_matrices_pd['timeperiod'] = payoff_matrices_timeperiod
     
-    #### strategy converted to norm ####
+    
     norms_to_store = []
     percent_time_frequency = []
     norms_candidates = trend_db_to_store.loc[trend_db_to_store['percent_count']>= norms_agents_frequency]
-    # norms_candidates = norms_candidates.replace({'strategy':thisdict2}) ### convert to numeric.
+    
     potential_norms_candidate = np.unique(norms_candidates['strategy']).tolist()
     for k in potential_norms_candidate:
         norms_candidates2 = norms_candidates.loc[norms_candidates['strategy']==k]
@@ -561,7 +587,7 @@ def simulation_function_neighbors(random_seed,
     
     
     
-    ##### distribution at the end of timeperiod.
+    
     color_map = []
     for j in range(len(agent_strategy_to_choose)):
         for i in range(len(names_to_check)):
@@ -575,29 +601,40 @@ def simulation_function_neighbors(random_seed,
     nx.draw(G,with_labels=True,node_color=color_map,labels=labels_for_graph)
     l,r = plt.xlim()
     plt.xlim(l-0.05,r+0.05)
-    # plt.show()
+    
     plt.savefig(path_to_save_output+"network_after_"+str(num_of_trials)+'_timeperiods_'+iteration_name+"_"+today+".png")
     plt.clf()
     
     
-    ###  trend graph
+    
     fig,ax = plt.subplots()
     data_for_trend_plot = trend_db_to_store.loc[trend_db_to_store['starting_position']==str([thisdict[k] for k in samplelist1])]
     data_for_trend_plot = data_for_trend_plot.reset_index(drop=True)
     for label,grp in data_for_trend_plot.groupby('strategy'):
         grp.plot(x='timeperiod_number',y='percent_count',ax=ax,label=label)
+    
+    
+    x_values11 = data_for_trend_plot['timeperiod_number'].unique()
+    total_periods11 = len(x_values11)
+    max_display_labels = 5  
+    interval11 = max(1, total_periods11 // max_display_labels)  
+
+    ax.set_xticks(np.arange(len(x_values11))[::interval11])
+    ax.set_xticklabels(x_values11[::interval11])
+    
+    
     ax.set_xlabel('Timeperiod')
     ax.set_ylabel('Count %')
-    # plt.show()
+    
     plt.savefig(path_to_save_output+"strategy_trend_"+iteration_name+"_"+today+".png")
     plt.clf()
     
     
-    ###### dataframe of strategy the timeperiod when it reached the norms stage #####
+    
     db_to_fill2 = pd.DataFrame()
     db_to_fill2["timeperiod"] = -1
     db_to_fill2["name_offered"] = -1
-    # data_for_norms_plot = trend_db_to_store.loc[trend_db_to_store['starting_position']==str([thisdict[k] for k in samplelist1])]
+    
     if len(norms_to_store) > 0:
         for j in norms_to_store:
             foocheck = data_for_trend_plot.loc[data_for_trend_plot["strategy"]==j]
@@ -644,3 +681,4 @@ def simulation_function_neighbors(random_seed,
     payoff_matrices_pd.to_excel(path_to_save_output+"payoff_matrices_considered_by_timeperiod_"+iteration_name+"_"+today+".xlsx",index=None)
 
     return(print("done"))
+
